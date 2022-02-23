796840986875ff9171066ab8611e604cdba40d78
Snowsore <snow_sore@hotmail.com>
2022-02-24 02:48:43 +0800

    Add file listing

--- routes/api/file.js
+++ routes/api/file.js
@@ -5,39 +5,37 @@ const fs = require("fs");
 const path = require("path");
 let spacePath = path.join(__dirname, "../../space/");
 
-function getAllFile(dir){
-  var list= {dir:{}}
-  function traverse(res,dir){
-    fs.readdirSync(dir).forEach((file)=>{
-      const pathname=path.join(dir,file)
-      if(fs.statSync(pathname).isDirectory()){
-        res.dir["file"] = {}
-        traverse(res.dir,pathname)
-      }else{
-        res.dir.join(file)
+function getAllFile(dir) {
+  var list = { dir: {} };
+  function traverse(res, dir) {
+    fs.readdirSync(dir).forEach((file) => {
+      const pathname = path.join(dir, file);
+      if (fs.statSync(pathname).isDirectory()) {
+        res.dir["file"] = {};
+        traverse(res.dir, pathname);
+      } else {
+        res.dir.join(file);
       }
-    })
+    });
   }
-  traverse(list,dir)
+  traverse(list, dir);
   return list;
 }
 
 router.get("/list", (req, res) => {
+  const folderPath = path.join(__dirname, "../../", req.query.dir);
   const list = fs
-    .readdirSync(spacePath, { withFileTypes: true })
+    .readdirSync(folderPath, { withFileTypes: true })
     .map((l) => (l.isDirectory() ? { ...l, name: l.name + "/" } : l));
   res.json(list);
-
 });
 
-router.post("/download", (req,res) => {
-  const list = req.body.downloadlist
-  for (file in list["file"])
-    res.download(path.join(spacePath,file))
+router.post("/download", (req, res) => {
+  const list = req.body.downloadlist;
+  for (file in list["file"]) res.download(path.join(spacePath, file));
   // for(dir in list["dir"])
   //   for(file in getAllFile(dir))
   //     res.download
-})
-
+});
 
 module.exports = router;
